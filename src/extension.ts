a5e9a563678ae36ef703e876d5da536bfe881ecd
pegasus.cadence@gmail.com <pegasus.cadence@gmail.com>
2024-10-24 03:15:14 -0700

    Init

---
+++ src/extension.ts
@@ -0,0 +1,78 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+    const taskProvider = vscode.tasks.registerTaskProvider('moon', {
+        provideTasks: () => {
+            return getCustomTasks();
+        },
+        resolveTask(_task: vscode.Task): vscode.Task | undefined {
+            return undefined;
+        }
+    });
+    context.subscriptions.push(taskProvider);
+}
+
+async function getCustomTasks(): Promise<vscode.Task[]> {
+    let tasks: vscode.Task[] = [];
+    const workspaceFolders = vscode.workspace.workspaceFolders;
+    // Retrieve the user-defined setting
+    const config = vscode.workspace.getConfiguration('myExtension');
+    const scanSubdirectoryForProject = config.get<boolean>('scanSubdirectoryForProject', true);  // Default to 'build.sh' if not set
+
+    if (workspaceFolders) {
+        // Iterate over each workspace folder
+        for (const folder of workspaceFolders) {
+			await asyncGetTasksForFolder(folder.uri, folder.name);
+			if (scanSubdirectoryForProject)	{
+				try {
+					// Get the contents of the workspace folder
+					const contents = await vscode.workspace.fs.readDirectory(folder.uri);
+	
+					// Iterate through each item and check if it's a directory
+					for (const [name, type] of contents) {
+						if (type === vscode.FileType.Directory) {
+							const subdirectoryUri = vscode.Uri.joinPath(folder.uri, name);
+							await asyncGetTasksForFolder(subdirectoryUri, name);
+						}
+					}
+				} catch (error) {
+					//vscode.window.showErrorMessage(`Error reading directory: ${folder.name}`);
+				}	
+			}
+		}
+	}
+    return tasks;
+
+	async function asyncGetTasksForFolder(folderUri: vscode.Uri, folderName: string) {
+		const signatureFileName = 'moon.mod.json';
+		const fileUri = vscode.Uri.joinPath(folderUri, signatureFileName); // Create the URI for the file
+
+		try {
+			// Try to get file stats
+			await vscode.workspace.fs.stat(fileUri);
+			{
+				const folderPath = folderUri.fsPath; // Get the absolute path of the folder
+
+				createTask(folderPath, 'build', vscode.TaskGroup.Build);
+				createTask(folderPath, 'test', vscode.TaskGroup.Test);
+				createTask(folderPath, 'clean', vscode.TaskGroup.Clean);
+			}
+		} catch (error) {
+			//vscode.window.showWarningMessage(`File "${signatureFileName}" does not exist in ${folderUri.fsPath}`);
+		}
+
+		function createTask(folderPath: string, scriptStr: string, taskGroup: vscode.TaskGroup) {
+			const task = new vscode.Task(
+				{ type: 'moon', script: scriptStr },
+				vscode.TaskScope.Workspace,
+				'moon ' + scriptStr + ' ' + folderName,
+				'moonbit',
+				new vscode.ShellExecution('moon ' + scriptStr, { cwd: folderPath })
+			);
+			task.group = taskGroup; // Adding task to the Test group
+			tasks.push(task);
+		}
+	}
+}
+
+export function deactivate() {}
